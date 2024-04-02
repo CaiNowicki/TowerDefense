8f72090e684029e4027666df7467c47f7944c11f
Cai Nowicki <cai.nowicki@gmail.com>
2024-04-01 22:08:33 -0400

    created main.py file and initialized a game window

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import pygame
+import os
+
+from pygame.locals import *
+
+pygame.init()
+window = pygame.display.set_mode((468,60))
+
+pygame.display.set_caption("Awesome Tower Defense")
+
+screen = pygame.display.get_surface()
+
